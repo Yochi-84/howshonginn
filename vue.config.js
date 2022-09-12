fc5d40174e34acae9e00fa1eb6f9e3b6ef04750d
Chi <yochi080684@gmail.com>
2022-09-12 20:44:28 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/'
+  transpileDependencies: true
 });
 
 module.exports = {
@@ -22,4 +19,7 @@ module.exports = {
       },
     },
   },
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/howshonginn/'
+  : './'
 };
